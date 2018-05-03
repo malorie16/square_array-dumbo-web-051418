e0b639ddaa4b77fdff9f442c9b6ed30bf53a2054
malorie16 <malorie18@gmail.com>
2018-05-03 14:17:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,60 +1,6 @@
 def square_array(numbers)
 
   numbers.each do |number|
-    puts numbers( Bold is where we want your affect, tone, and energy to sound like)
-( Sharing the hear say, “gossip” as if talking to friend )
-Something just happened in the town of Cold Spring.
-0:00-0:06
-( Pull the individual in closer, approach.. more in tone where it’s giving more of lets move
-over here for more privacy)
-Come here.
-0:06-0:07
-( exciting times, charismatic )
-We're opening a new audition for another role for the show, SHORT BUS.
-0:08-0:11
-( Legit, no smoke.. for real)
-It's a big role.
-0:13-0:14
-( revealing with question then rise with excitement )
-who? The role of Zoey... the main character!
-0:14-18
-( overdramatic sadness with confusion)
-I thought that was supposed to be Millie Simmonds??
-0:18-0:23
-(Tone of support and props)
-That girl's on a roll.
-0:24-0:25
-(Secretive and yet excited)
-You've heard that A QUIET PLACE is getting a sequel, right?
-0:26-0:28
-( confident and yet supportive )
-Millie's a rising star. Life has other plans for her, so she isn't able to take part in this production.
-0:29-0:34
-(Genuine, humble and respectful)
-But we're so proud of Millie and wish her the best of luck with her continuing success.
-0:35-0:40
-( serious, undertone of urgency)
-Now, what does this mean for you?
-0:41-0:42
-( smooth, confident and on a mission)
-Assuming you are a person that's deaf or hard of hearing and look between the age of 13 to 16.
-0:43-0:48
-( over the top dramatic drum roll of introducing a star)
-This is an opportunity for you to become the new Zoey, star of this show.
-0:50-0:55
-( exposed their insecurities in a got busted kind of way)
-It's hard than it looks, okay??
-1:02-1:04
-( Professional and confident with good energy )
-Auditions are now open for 3 characters, no, 4 characters—including Zoey.
-1:07-1:13
-( Professional and calm, confident)
-The four characters are close friends but each have their own unique personality. More
-information on the characters, how to audition, and deadlines... all information can be found on
-our website.
-1:14-1:26
-( inviting, smooth hip)
-Come and join the short bus!
-1:27-1:28 
+    puts numbers
   end
 end
