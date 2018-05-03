2a4aa789e7f8f5b1d8e7a060a3b66e42a4df6764
malorie16 <malorie18@gmail.com>
2018-05-03 14:32:02 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 numbers = [1, 2, 3]
 
 def square_array(array)
-  array = numbers.each do |number|
-      puts number * number
-  end
+  
 array
 end
+
 array = numbers.each do |number|
     puts number * number
 end
